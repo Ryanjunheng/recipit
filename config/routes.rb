4f4cae793b9ba6b048527a89ad2d7fd0a53d7397
Ryan <junheng102@gmail.com>
2018-01-26 11:45:57 +0800

    set up simple user authentication using bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 	root "home#index"
 	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  	get "/sign_up" => "users#new", as: "sign_up"
+  	get "/sign_in" => "sessions#new", as: "sign_in"
+  	post "/login" => "sessions#create", as: "login"
+  	delete "/sign_out" => "sessions#destroy", as: "sign_out"
+
 end
