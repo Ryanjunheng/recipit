f64fae1914f04eaa649bd7a2ebe0a6c2c7db2d8f
Ryan <junheng102@gmail.com>
2018-01-28 18:36:53 +0800

    add open weather api to show weather on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,13 @@ class HomeController < ApplicationController
 
 	def index
 		@recipe = Recipe.includes(:user).order(:created_at).page params[:page]
+
+		resp = RestClient.get("http://api.openweathermap.org/data/2.5/weather?q=kuala%20lumpur&appid=5261318cfb0bef89c5636df1236c3240")
+  		hash = JSON.parse(resp.body)
+  		@weather = hash["weather"][0]["description"]
+  		@city = hash["name"]
+  		@icon = hash["weather"][0]["icon"]
+  		@temp = (hash["main"]["temp"] - 273.15).round(1) 
 	end
 
 	def search
